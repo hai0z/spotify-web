fa43e94703294da6a8393ae36230a12e88ccc6f2
hai0z <hainguyen56211@gmail.com>
2023-06-25 12:22:47 +0700

    coi nhu xong

--- src/components/SongCard/MiniCard.jsx
+++ src/components/SongCard/MiniCard.jsx
@@ -4,28 +4,32 @@ import { usePlayerContext } from "../../context/PlayerProvider";
 function MiniCard({ item, onClick }) {
     const { currentSong, isPlaying } = usePlayerContext();
     return (
-        <div className="flex flex-row bg-[#b3b3b380] items-center rounded-md cursor-pointer justify-between group">
-            <div className="flex flex-row items-center ">
+        <div className="flex flex-row w-full bg-[#b3b3b380] items-center rounded-md cursor-pointer justify-between group">
+            <div className="flex flex-row items-center w-full">
                 <img
                     src={item?.images?.coverart}
                     alt="playyyy"
                     className="object-cover h-20 w-20 rounded-tl-md rounded-bl-md"
                 />
-                <span className="text-[16px] text-white font-bold ml-2 w-40 overflow-hidden block truncate">
+                <span className="text-[16px] text-white font-bold ml-2  line-clamp-2">
                     {item?.title}
                 </span>
             </div>
-            <div
-                className={`bg-[#1fdf64] w-12 h-12 rounded-full mr-4  justify-center items-center  transition-all duration-200 opacity-0 group-hover:opacity-100 flex hover:scale-105 shadow-sm ${
-                    currentSong?.key === item?.key && isPlaying && "opacity-100"
-                }`}
-                onClick={onClick}
-            >
-                {isPlaying && item?.key === currentSong?.key ? (
-                    <BsFillPauseFill className="text-[#000] text-[24px]" />
-                ) : (
-                    <BsFillPlayFill className="text-[#000] text-[24px]" />
-                )}
+            <div>
+                <div
+                    className={`bg-[#1fdf64] w-12 h-12 rounded-full mr-4  justify-center items-center  transition-all duration-200 opacity-0 group-hover:opacity-100 flex hover:scale-105 shadow-sm ${
+                        currentSong?.key === item?.key &&
+                        isPlaying &&
+                        "opacity-100"
+                    }`}
+                    onClick={onClick}
+                >
+                    {isPlaying && item?.key === currentSong?.key ? (
+                        <BsFillPauseFill className="text-[#000] text-[24px]" />
+                    ) : (
+                        <BsFillPlayFill className="text-[#000] text-[24px]" />
+                    )}
+                </div>
             </div>
         </div>
     );
